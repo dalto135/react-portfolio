55928972640a176b9491f52dddb9665139cf7414
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-06-30 01:38:54 -0400

    project components now populated with projects object, working on way to map through projects object w/o disrupting css

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,41 +10,6 @@ import Screenshot6 from '../assets/images/coding-quiz-challenge.png';
 
 
 function Projects() {
-    let titles = [
-        'Global Lost and Found',
-        'Dine or Dash',
-        'Tech Blog',
-        'Employee Tracker',
-        'Weather Dashboard',
-        'Coding Quiz Challenge',
-    ];
-
-    let links = [
-        'https://glacial-fortress-45860.herokuapp.com/',
-        'https://dalto135.github.io/dine-or-dash/',
-        'https://intense-sea-31464.herokuapp.com/',
-        'https://drive.google.com/file/d/1PRgjvLbEubjJbmaixVOPNbvKF6S-0zwB/view',
-        'https://dalto135.github.io/weather-dashboard/',
-        'https://dalto135.github.io/coding-quiz-challenge/',
-    ];
-
-    let descriptions = [
-        'A web application that allows users to post and search for items that have been reported missing. Technologies used include Sequelize, Javascript, Handlebars, and MYSQL2.',
-        'A webpage that prompts the user to enter a food dish and location, and displays a list of recipies and local restaurants that match their dish. Technologies used include HTML, CSS, Javascript, and API integration.',
-        'A social networking website that allows users to create an account, make blog posts, and leave comments on posts others have made. Users can go to their personal dashboards, where they can see a list of posts they have made and are prompted to make new posts.',
-        'A program that uses Node and Inquirer to manipulate a SQL database of employees, departments and employee roles. Technologies used include Javascript, Node.js, Inquirer, and Dotenv.',
-        'A webpage that prompts the user to enter a city name, and displays current weather conditions for that city in addition to a 5 day forecast. Technologies used include HTML, CSS, Javascript, and API integration.',
-        'A web application that presents an interactive Javascript quiz. The quiz is timed and time is deducted if a question is answered wrong. The quiz ends when all questions are answered or the timer runs out, and the user can store their score after typing in their initials.',
-    ];
-
-    let githubLinks = [
-        'https://github.com/Youngin9210/Lost-Found',
-        'https://github.com/dalto135/dine-or-dash',
-        'https://github.com/dalto135/tech-blog',
-        'https://github.com/dalto135/employee-tracker',
-        'https://github.com/dalto135/weather-dashboard',
-        'https://github.com/dalto135/coding-quiz-challenge',
-    ];
 
     let projects = [
         {
@@ -98,36 +63,36 @@ function Projects() {
             </div>
             
             <div className="content" id="work">
+
+                {/* {projects.map((project, i) => 
+                        <Project key={i} title={project.title} link={project.link} screenshot={project.screenshot} description={project.description} github={project.github}/>
+                )} */}
                 
                 {/* Row one projects */}
                 <div className='big'>
 
-                    {/* {projects.map((project, i) => 
-                        <Project key={i} title={project.title} link={project.link} screenshot={project.screenshot} description={project.description} github={project.github}/>
-                    )} */}
+                    <Project title={projects[0].title} link={projects[0].link} screenshot={projects[0].screenshot} description={projects[0].description} github={projects[0].github}/>
 
-                    <Project title={titles[0]} link={links[0]} screenshot={projects[0].screenshot} description={descriptions[0]} githubLink={githubLinks[0]}/>
+                    <Project title={projects[1].title} link={projects[1].link} screenshot={projects[1].screenshot} description={projects[1].description} github={projects[1].github}/>
 
-                    <Project title={titles[1]} link={links[1]} screenshot={projects[1].screenshot} description={descriptions[1]} githubLink={githubLinks[1]}/>
-               
                 </div>
                 
                 {/* Row two projects */}
                 <div className="big">
 
-                    <Project title={titles[2]} link={links[2]} screenshot={projects[2].screenshot} description={descriptions[2]} githubLink={githubLinks[2]}/>
+                    <Project title={projects[2].title} link={projects[2].link} screenshot={projects[2].screenshot} description={projects[2].description} github={projects[2].github}/>
 
-                    <Project title={titles[3]} link={links[3]} screenshot={projects[3].screenshot} description={descriptions[3]} githubLink={githubLinks[3]}/>
-                    
+                    <Project title={projects[3].title} link={projects[3].link} screenshot={projects[3].screenshot} description={projects[3].description} github={projects[3].github}/>
+
                 </div>
 
                 {/* /* Row three projects */}
                 
                 <div className="big">
 
-                    <Project title={titles[4]} link={links[4]} screenshot={projects[4].screenshot} description={descriptions[4]} githubLink={githubLinks[4]}/>
+                    <Project title={projects[4].title} link={projects[4].link} screenshot={projects[4].screenshot} description={projects[4].description} github={projects[4].github}/>
 
-                    <Project title={titles[5]} link={links[5]} screenshot={projects[5].screenshot} description={descriptions[5]} githubLink={githubLinks[5]}/>
+                    <Project title={projects[5].title} link={projects[5].link} screenshot={projects[5].screenshot} description={projects[5].description} github={projects[5].github}/>
 
                 </div>
                 
