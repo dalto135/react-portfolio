fd6ae9fcecbe9787e420a6512bee6e30afd00c87
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-06-30 02:50:45 -0400

    put projects in the desired order

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,75 +1,79 @@
 import React from 'react';
 import Project from '../components/project';
 
-import Screenshot1 from '../assets/images/global-lost-and-found.png';
-import Screenshot2 from '../assets/images/dine-or-dash.png';
-import Screenshot3 from '../assets/images/tech-blog.png';
-import Screenshot4 from '../assets/images/employee-tracker.png';
-import Screenshot5 from '../assets/images/weather-dashboard.png';
-import Screenshot6 from '../assets/images/coding-quiz-challenge.png';
-import Screenshot7 from '../assets/images/bread-exchange.png';
-import Screenshot8 from '../assets/images/google-books-search.png';
+import Screenshot1 from '../assets/images/bread-exchange.png';
+import Screenshot2 from '../assets/images/global-lost-and-found.png';
+import Screenshot3 from '../assets/images/dine-or-dash.png';
+import Screenshot4 from '../assets/images/google-books-search.png';
+import Screenshot5 from '../assets/images/tech-blog.png';
+import Screenshot6 from '../assets/images/employee-tracker.png';
+import Screenshot7 from '../assets/images/weather-dashboard.png';
+import Screenshot8 from '../assets/images/coding-quiz-challenge.png';
+
+
 
 
 function Projects() {
 
     let projects = [
         {
+            title: 'Bread Exchange',
+            link: 'https://bread-exchange.herokuapp.com/',
+            screenshot: Screenshot1,
+            description: 'Bread exchange is a webpage using the MERN stack which aims to decrease food waste and bring the community together. We allow individuals and businesses to make their foods available on our site, and connect with others in the community who are experiencing food shortages.',
+            github: 'https://github.com/dalto135/bread-exchange'
+        },
+        {
             title: 'Global Lost and Found',
             link: 'https://glacial-fortress-45860.herokuapp.com/',
-            screenshot: Screenshot1,
+            screenshot: Screenshot2,
             description: 'A web application that allows users to post and search for items that have been reported missing. Technologies used include Sequelize, Javascript, Handlebars, and MYSQL2.',
             github: 'https://github.com/Youngin9210/Lost-Found',
         },
         {
             title: 'Dine or Dash',
             link: 'https://sarehontaylor.github.io/DineOrDash/',
-            screenshot: Screenshot2,
+            screenshot: Screenshot3,
             description: 'A webpage that prompts the user to enter a food dish and location, and displays a list of recipies and local restaurants that match their dish. Technologies used include HTML, CSS, Javascript, and API integration.',
             github: 'https://github.com/SarehonTaylor/DineOrDash'
         },
         {
+            title: 'Google Books Search',
+            link: 'https://sheltered-ocean-65878.herokuapp.com/',
+            screenshot: Screenshot4,
+            description: 'A react webpage that uses the Google Books API to provide a search engine for all of the books Google has made available. Users can make searches to access Google Books and add books to their favorites.',
+            github: 'https://github.com/dalto135/google-books-search'
+        },
+        {
             title: 'Tech Blog',
             link: 'https://intense-sea-31464.herokuapp.com/',
-            screenshot: Screenshot3,
+            screenshot: Screenshot5,
             description: 'A social networking website that allows users to create an account, make blog posts, and leave comments on posts others have made. Users can go to their personal dashboards, where they can see a list of posts they have made and are prompted to make new posts.',
             github: 'https://github.com/dalto135/tech-blog'
         },
         {
             title: 'Employee Tracker',
             link: 'https://drive.google.com/file/d/1PRgjvLbEubjJbmaixVOPNbvKF6S-0zwB/view',
-            screenshot: Screenshot4,
+            screenshot: Screenshot6,
             description: 'A program that uses Node and Inquirer to manipulate a SQL database of employees, departments and employee roles. Technologies used include Javascript, Node.js, Inquirer, and Dotenv.',
             github: 'https://github.com/dalto135/employee-tracker'
         },
         {
             title: 'Weather Dashboard',
             link: 'https://dalto135.github.io/weather-dashboard/',
-            screenshot: Screenshot5,
+            screenshot: Screenshot7,
             description: 'A webpage that prompts the user to enter a city name, and displays current weather conditions for that city in addition to a 5 day forecast. Technologies used include HTML, CSS, Javascript, and API integration.',
             github: 'https://github.com/dalto135/weather-dashboard'
         },
         {
             title: 'Coding Quiz Challenge',
             link: 'https://dalto135.github.io/coding-quiz-challenge/',
-            screenshot: Screenshot6,
+            screenshot: Screenshot8,
             description: 'A web application that presents an interactive Javascript quiz. The quiz is timed and time is deducted if a question is answered wrong. The quiz ends when all questions are answered or the timer runs out, and the user can store their score after typing in their initials.',
             github: 'https://github.com/dalto135/coding-quiz-challenge'
         },
-        {
-            title: 'Bread Exchange',
-            link: 'https://bread-exchange.herokuapp.com/',
-            screenshot: Screenshot7,
-            description: 'Bread exchange is a webpage using the MERN stack which aims to decrease food waste and bring the community together. We allow individuals and businesses to make their foods available on our site, and connect with others in the community who are experiencing food shortages.',
-            github: 'https://github.com/dalto135/bread-exchange'
-        },
-        {
-            title: 'Google Books Search',
-            link: 'https://sheltered-ocean-65878.herokuapp.com/',
-            screenshot: Screenshot8,
-            description: 'A react webpage that uses the Google Books API to provide a search engine for all of the books Google has made available. Users can make searches to access Google Books and add books to their favorites.',
-            github: 'https://github.com/dalto135/google-books-search'
-        },
+        
+       
     ];
 
     return (
@@ -112,13 +116,13 @@ function Projects() {
                 </div>
 
                 {/* /* Row four projects */}
-                <div className="big">
+                {/* <div className="big">
 
                     <Project title={projects[6].title} link={projects[6].link} screenshot={projects[6].screenshot} description={projects[6].description} github={projects[6].github}/>
 
                     <Project title={projects[7].title} link={projects[7].link} screenshot={projects[7].screenshot} description={projects[7].description} github={projects[7].github}/>
 
-                </div>
+                </div> */}
                 
             </div>
             
