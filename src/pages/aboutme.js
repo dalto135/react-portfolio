6fbc854e9596f99b6d1d195424618f9af6e66269
Dalton Wilkins <wilkins.271@osu.edu>
2022-05-20 18:13:10 -0400

    Change job title

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -9,7 +9,7 @@ function Aboutme() {
             </div>
             
             <div className="content" id="me">
-                <p>Highly motivated and technically skilled Full-stack Developer at Nationwide. Has a certificate from the Full-stack Web Development Bootcamp at The Ohio State University and a Bachelor’s degree in Neuroscience from The Ohio State University. Enjoys working with others to solve complex problems using development skills that include HTML, CSS, Javascript, SQL, Git, React.js and others, and possesses soft skills that include conflict resolution, effective communication, flexibility and humility in leadership. These experiences and certifications have given me the drive and analytical skills needed to succeed in my Software Development career and beyond.</p>
+                <p>Highly motivated and technically skilled Software Engineer at Nationwide. Has a certificate from the Full-stack Web Development Bootcamp at The Ohio State University and a Bachelor’s degree in Neuroscience from The Ohio State University. Enjoys working with others to solve complex problems using development skills that include HTML, CSS, Javascript, SQL, Git, React.js and others, and possesses soft skills that include conflict resolution, effective communication, flexibility and humility in leadership. These experiences and certifications have given me the drive and analytical skills needed to succeed in my Software Engineering career and beyond.</p>
             </div>
         </section>
     );
