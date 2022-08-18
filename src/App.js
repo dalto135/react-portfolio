fba6632a3277c9a75558677aebff01423f2a1cb0
dalto135 <67397294+dalto135@users.noreply.github.com>
2022-08-17 20:05:15 -0400

    bulk of styling done for light mode, still working to ma ke button functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import Header from './components/header';
 import Aboutme from './pages/aboutme';
@@ -8,15 +8,30 @@ import Footer from './components/footer';
 import './style.css';
 // import './styleLightMode.css';
 
+export const themeContext = createContext(null);
+
 function App() {
+
+  const [theme, setTheme] = useState("dark");
+  // const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  }
+
   return (
-    <Router>
-      <Header/>
-      <Route exact path='/' component={Aboutme}></Route>
-      <Route exact path='/projects' component={Projects}></Route>
-      <Route exact path='/contactme' component={Contactme}></Route>
-      <Footer/>
-    </Router>
+    <themeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={ theme }>
+        <Router>
+          <Header/>
+          <Route exact path='/' component={Aboutme}></Route>
+          <Route exact path='/projects' component={Projects}></Route>
+          <Route exact path='/contactme' component={Contactme}></Route>
+          <Footer/>
+        </Router>
+      </div>
+    </themeContext.Provider>
+    
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -3,43 +3,35 @@ import { Link } from 'react-router-dom';
 import Headshot from '../assets/images/headshot.jpg';
 import './components.css';
 import './header.css';
+import toggleTheme from '../App'
 
 function Header() {
     return (
         <div id='header-div'>
             <header>
-
                 <div id="name">
                     <h1>Dalton Wilkins</h1>
                 </div>
                 
                 <div className='headshot'>
                     <img src={Headshot} alt='headshot'/>
-                    <form>
-                        <label>Dark Mode</label>
-                        <input type="checkbox" id="darkmode" checked/>
-                    </form>
-                    
+
                     <div id="shortcuts">
                         <Link to='/'>About Me</Link>
                         <Link to='/projects'>Projects</Link>
                         <Link to='/contactme'>Contact Me</Link>
-                        <a href='https://drive.google.com/file/d/19fEXc-UwH_rd4qKfTBPd_yd3_9uusd6c/view?usp=sharing' target='_blank' rel='noreferrer'>
-                            Resume
-                        </a>
+                        <a href='https://drive.google.com/file/d/19fEXc-UwH_rd4qKfTBPd_yd3_9uusd6c/view?usp=sharing' target='_blank' rel='noreferrer'>Resume</a>
                         {/* <a href='https://drive.google.com/file/d/14IgY0ERipLtSLQX5bAk-OVvs_JuXVkxk/view?usp=sharing' target='_blank' rel='noreferrer'>Brand Statement</a> */}
                     </div>
-                </div>
 
+                    {/* <button onClick={toggleTheme} id="lightDarkButton">Light/Dark Mode</button> */}
+                </div>
             </header>
 
             <section id="image">
                 <h1>A vision for the future</h1>
             </section>
-                
-            
         </div>
-
     );
 }
 
